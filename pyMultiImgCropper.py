41a882d89a469ce1cb92532bd8cd4ec014357055
mhr380 <hajimemihara1108@gmail.com>
2016-10-16 23:12:45 +0900

    modefy img file structure

--- pyMultiImgCropper.py
+++ pyMultiImgCropper.py
@@ -52,6 +52,7 @@ class MainWidget(QtGui.QWidget):
         self.end_pt     = [0, 0]
 
         self.flg_square = False
+        self.flg_allowCrop = False
 
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key_B:
@@ -79,11 +80,16 @@ class MainWidget(QtGui.QWidget):
             event.ignore()
 
     def dropEvent(self, event):
+
+        self.flg_allowCrop = True
         self.urls = event.mimeData().urls()
         self.file_num = len(self.urls)
 
         self.path_list = [str(url.path()[1:]) for url in self.urls]
         self.img_num = len(self.path_list)
+        
+        self.img_list = [cv2.imread(self.path_list[num], 1) for num in range(self.img_num)]
+        
         self.showImg()
 
     def mousePressEvent(self, event):
@@ -98,6 +104,7 @@ class MainWidget(QtGui.QWidget):
         self.pressed = False
         self.end_pt = event.pos().x(), event.pos().y()
         self.update()
+        self.saveCroppedImage()
     
     def paintEvent(self, event):
         painter = QtGui.QPainter()
@@ -126,13 +133,14 @@ class MainWidget(QtGui.QWidget):
         painter.end()
 
     def showImg(self, num=0):
-        img = cv2.imread(self.path_list[num], 1)
+        
+        img = self.img_list[num]
         h, w, ch = img.shape
 
         self.resize(w, h)
         bytesPerLine = 3 * w
             
-        cv2.cvtColor(img, cv2.COLOR_BGR2RGB, img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         self.image = QtGui.QImage(img.data, w, h, bytesPerLine, QtGui.QImage.Format_RGB888)
 
@@ -140,8 +148,16 @@ class MainWidget(QtGui.QWidget):
         self.pixlabel.resize(self.image.size())
     
     def saveCroppedImage(self):
-        img_list = []
+        img_list = [cv2.imread(self.path_list[num], 1) for num in range(self.img_num)]
+        
+        ipx = self.initial_pt[0]
+        ipy = self.initial_pt[1]
+        epx = self.end_pt[0]
+        epy = self.end_pt[1] 
 
+        if self.flg_allowCrop:
+            for n, img in enumerate(img_list):
+                cv2.imwrite(self.path_list[n], img[ ipy: epy, ipx: epx, :])
     
 if __name__ == "__main__":
         
